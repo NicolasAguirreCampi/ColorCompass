cdc5226e01f5b4854c62036c733e0f96c900b4b6
Nicolas Aguirre <aguirrecampi.nicolas@gmail.com>
2023-10-13 10:43:34 -0300

    feat: restructured project to be able to upload to pypi

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='colorcompass',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Nicolas Aguirre Campi',
+    author_email='aguirrecampi.nicolas@gmail.com',
+    description='A brief description of your project.',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/NicolasAguirreCampi/ColorCompass',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
