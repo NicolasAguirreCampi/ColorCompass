0b096f7e6a59ce3f554c433a0a25490214fa3e21
Nicolas Aguirre <aguirrecampi.nicolas@gmail.com>
2023-10-18 08:33:37 -0300

    feat: updated version to 1.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='colorcompass',
-    version='1.2.0',
+    version='1.2.1',
     packages=find_packages(),
     install_requires=[],
     author='Nicolas Aguirre Campi',
